7ea9a6103f39df0579d88312901df38fd731be9b
Maximilian124 <126184774+Maximilian124@users.noreply.github.com>
2023-04-19 16:08:25 +0300

    changes

--- Raspi.py
+++ Raspi.py
@@ -63,35 +63,51 @@ def get_humidity():
 
 
 # Function to determine the window position based on the current weather data
-def get_window_position(weather_data):
-
+def get_window_position(weather_data, temperature, humidity):
     weather_conditions = weather_data['weather'][0]['main']
     if weather_conditions == 'Rain' or weather_conditions == 'Snow':
         return tilt_angle
     elif weather_conditions == 'Thunderstorm':
         return closed_angle
-    else:
+    elif temperature > temp_threshold or humidity > humidity_threshold:
         return open_angle
+    else:
+        return closed_angle
+
 
 
 # Set the initial window position to closed
 current_position = closed_angle
+
 # Main loop
 while True:
-    # Check if the temperature or humidity exceeds the thresholds
-    if float(get_temperature()) > float(temp_threshold) or float(get_humidity()) > float(humidity_threshold):
-        window_position = get_window_position(get_weather_data())
+    temperature = get_temperature()
+    humidity = get_humidity()
+    weather_data = get_weather_data()
+    # Check if the temperature or humidity exceeds the thresholds or if it's raining or snowing
+    if temperature > temp_threshold or humidity > humidity_threshold or weather_data['weather'][0]['main'] == 'Rain' or weather_data['weather'][0]['main'] == 'Snow':
+        window_position = get_window_position(weather_data, temperature, humidity)
         # Check if the desired position is different from the current position
         if window_position != current_position:
-            # Adjust the servo to the new position
+            # Rotate the servo through all angles in increments of 1 second until it reaches 360 degrees
+            for i in range(0, 360, 1):
+                angle = i % 90  # Limit the angle to 90 degrees
+                duty_cycle = 2 + (angle / 18)
+                pwm.ChangeDutyCycle(duty_cycle)
+                time.sleep(1)
+
+            # Set the servo to the desired position
             GPIO.output(servo_pin, True)
-            pwm.ChangeDutyCycle(2 + (window_position / 18))
+            duty_cycle = 2 + (window_position / 18)
+            pwm.ChangeDutyCycle(duty_cycle)
             print("Window position: " + str(window_position) + " degrees")
             time.sleep(1)
             GPIO.output(servo_pin, False)
             pwm.ChangeDutyCycle(0)
+
             # Update the current position
             current_position = window_position
+
     else:
         # Check if the desired position is different from the current position
         if current_position != closed_angle:
@@ -105,3 +121,6 @@ while True:
             print("Window closed")
             # Update the current position
             current_position = closed_angle
+
+    # Wait for some time before checking the conditions again
+    time.sleep(5)
